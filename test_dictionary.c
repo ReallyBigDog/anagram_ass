045f4b01121371650d20c0a33e791f91afdcda0f
Eli Garcia <eligarcia@Elis-MacBook-Pro.local>
2019-04-19 21:51:03 -0400

    ReallyBigDog is done

--- test_dictionary.c
+++ test_dictionary.c
@@ -12,18 +12,22 @@ void print_list(VNode *head){
 	printf("\n");
 }
 
+int compare(const void *l1, const void *l2) {
+	return *(char*)l1 - *(char*)l2;
+}
+
 int main (int argc, char ** argv) {
-	
+	/*
 	DNode* result;
-	
+
 	//either static or use calloc - to set all bytes initially to 0
-	static DNode* dictionary [DEFAULT_HASH_SIZE]; 
-	
-	
+	static DNode* dictionary [DEFAULT_HASH_SIZE];
+
+
 	set(dictionary, DEFAULT_HASH_SIZE, "pore", "repo");
 	set(dictionary, DEFAULT_HASH_SIZE, "pore", "rope");
-	
-	
+
+
 	result = get (dictionary, DEFAULT_HASH_SIZE, "pore");
 	if (result != NULL) {
 		printf("Anagrams for 'pore':\n");
@@ -31,7 +35,7 @@ int main (int argc, char ** argv) {
 	}
 	else
 		printf ("'pore' not found\n");
-	
+
 	set(dictionary, DEFAULT_HASH_SIZE, "bore", "robe");
 	result = get (dictionary, DEFAULT_HASH_SIZE, "bore");
 	if (result != NULL) {
@@ -40,9 +44,41 @@ int main (int argc, char ** argv) {
 	}
 	else
 		printf ("'bore' not found\n");
-	
+
+	free_dictionary(dictionary, DEFAULT_HASH_SIZE);*/
+
+	DNode* result;
+	static DNode* dictionary [DEFAULT_HASH_SIZE];
+
+	FILE *f = fopen("words.txt", "r");
+	char buffer[25];
+	while (fscanf(f, "%s", buffer) != EOF) {
+		char* word = copystr(buffer);
+		qsort(word, strlen(word), sizeof(char), compare);
+		set(dictionary, DEFAULT_HASH_SIZE, word, buffer);
+
+		free(word);
+	}
+
+	int total = 0;
+	for (int i = 0; i < DEFAULT_HASH_SIZE; i++) {
+		if (dictionary[i] != NULL) {
+			result = get(dictionary, DEFAULT_HASH_SIZE, dictionary[i]->key);
+			VNode* head = (result->values)->next;
+
+			if (head) {
+				printf("%s\n", (result->values)->value);
+				while (head) {
+					printf("%s\n\n", head->value);
+					head = head->next;
+				}
+				total++;
+			}
+		}
+	}
+
+	printf("%d\n", total);
 	free_dictionary(dictionary, DEFAULT_HASH_SIZE);
 
-	
 	return 0;
 }
